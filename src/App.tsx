3406c7b7d320c356af278853503a05620b4d0ac2
Balázs <balazs@otakomaiya.com>
2023-11-17 23:53:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import styled from "styled-components";
 
 function App() {
-  return (
-    <>
-        <StyledButton
-            type="button"
-        >
-            Button text
-        </StyledButton>
-    </>
-  )
+    const createCheckout = async () => {
+        const response = await fetch('https://shaver-hungary.myshopify.com/api/2023-10/graphql.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Storefront-Access-Token': '77690c5d675bf03912d4c4e0e95572c1',
+            },
+            body: JSON.stringify({
+                query: `mutation {
+                    checkoutCreate(input: {
+                      lineItems: [{variantId: "43903146459401", quantity: 1}]
+                    }) {
+                      checkout {
+                        id
+                        webUrl
+                      }
+                    }
+                  }`,
+            }),
+        });
+
+        const responseData = await response.json();
+        console.log(responseData.data.checkoutCreate.checkout)
+    };
+
+    return (
+        <>
+            <StyledButton
+                type="button"
+                onClick={createCheckout}
+            >
+                Button text
+            </StyledButton>
+        </>
+    )
 }
 
 const StyledButton = styled.button`
