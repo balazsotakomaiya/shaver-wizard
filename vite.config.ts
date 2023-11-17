2d7efc0e4767635954336da13fc5f5e1e533ed44
Balázs <balazs@otakomaiya.com>
2023-11-17 19:41:24 +0100

    Lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'Wizard',
+      fileName: 'shaver-wizard',
+    },
+  }
 })
